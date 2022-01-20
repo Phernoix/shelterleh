7215109519c282cc94920baafd683b3655666b11
Phernoix <eklaseboer@gmail.com>
2022-01-20 15:17:00 +0800

    search(not done yet)

---
+++ app/src/main/java/com/sp/shelterleh/MapSearch.java
@@ -0,0 +1,14 @@
+package com.sp.shelterleh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_search);
+    }
+}
