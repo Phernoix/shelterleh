7480d62389aed4d72c78bf59298c614be02d7226
Phernoix <eklaseboer@gmail.com>
2022-02-10 17:29:45 +0800

    help fragment almost done

--- app/src/main/java/com/sp/shelterleh/HelpFragment.java
+++ app/src/main/java/com/sp/shelterleh/HelpFragment.java
@@ -13,5 +13,7 @@ public class HelpFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_help,container,false);   }
+        View v = inflater.inflate(R.layout.fragment_help,container,false);
+    return v;
+    }
 }
